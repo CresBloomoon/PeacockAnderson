b8daa10f96f273b8f59f670c2531ad431dbffccb
CresBloomoon <metroidm2421@gmail.com>
2022-12-04 22:02:14 +0900

    1st commit

---
+++ DependencyInjection/DependencyInjection.WinForm/Objects/ProductSqlServer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjection.WinForm.Objects
+{
+    public sealed class ProductSqlServer
+    {
+    }
+}
